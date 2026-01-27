3b57f8ec70f709e95f76e72f099de507456933c0
rachad <rachdos76@gmail.com>
2026-01-27 12:38:24 +0100

    ajouter les champs necessaire dans le modal recruteur

--- app/Models/ProfilRecruteur.php
+++ app/Models/ProfilRecruteur.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProfilRecruteur extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'nom_entreprise',
+        'description_entreprise',
+        'site_web',
+        'localisation'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
