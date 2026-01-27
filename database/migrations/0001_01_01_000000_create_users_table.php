f82a6996058f5f8b1b9493c710fb12bcecd187fc
rachad <rachdos76@gmail.com>
2026-01-27 12:07:51 +0100

    modifie migration de users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['recruteur', 'candidat']);
+            $table->string('photo')->nullable();
+            $table->text('biographie')->nullable();
+            $table->string('specialite')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
