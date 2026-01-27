4ec02eb8d50fcf132d64a514da6485354a73ba4b
rachad <rachdos76@gmail.com>
2026-01-27 18:28:49 +0100

    ajouter la partie inscription dans le controlleur

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProfilCandidat;
+use App\Models\ProfilRecruteur;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -30,25 +32,45 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-    'nom' => 'required|string|max:255',
-    'email' => 'required|email|unique:users',
-    'password' => 'required|confirmed|min:8',
-    'role' => 'required|in:recruteur,candidat',
+            'nom' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+            'role' => 'required|in:recruteur,candidat',
 
-    // Recruteur
-    'nom_entreprise' => 'required_if:role,recruteur',
-    'localisation' => 'required_if:role,recruteur',
+            // Recruteur
+            'nom_entreprise' => 'required_if:role,recruteur',
+            'localisation' => 'required_if:role,recruteur',
 
-    // Candidat
-    'specialite' => 'required_if:role,candidat',
-    'annees_experience' => 'required_if:role,candidat|integer|min:0',
-]);
+            // Candidat
+            'specialite' => 'required_if:role,candidat',
+            'annees_experience' => 'required_if:role,candidat|integer|min:0',
+        ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'nom' => $request->nom,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => $request->role,
         ]);
+
+        if ($request->role === 'recruteur') {
+            ProfilRecruteur::create([
+                'user_id' => $user->id,
+                'nom_entreprise' => $request->nom_entreprise,
+                'description_entreprise' => $request->description_entreprise,
+                'site_web' => $request->site_web,
+                'localisation' => $request->localisation,
+            ]);
+        }
+
+        if ($request->role === 'candidat') {
+            ProfilCandidat::create([
+                'user_id' => $user->id,
+                'specialite' => $request->specialite,
+                'annees_experience' => $request->annees_experience,
+                'competences' => $request->competences,
+            ]);
+        }
 
         event(new Registered($user));
 
