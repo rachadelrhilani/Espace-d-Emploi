5a9c13ba4d89a5309b9fa05bc193fbda6d417d26
rachad <rachdos76@gmail.com>
2026-01-27 14:28:41 +0100

    creer les champs necessaire pour le modal amitie

--- app/Models/Amitie.php
+++ app/Models/Amitie.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Amitie extends Model
 {
-    //
+    protected $fillable = [
+        'id_expediteur',
+        'id_destinataire',
+        'statut'
+    ];
+
+    public function expediteur()
+    {
+        return $this->belongsTo(User::class, 'id_expediteur');
+    }
+
+    public function destinataire()
+    {
+        return $this->belongsTo(User::class, 'id_destinataire');
+    }
 }
