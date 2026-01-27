e401ad5543613f36c2116dbfd819ee2d43a1daae
rachad <rachdos76@gmail.com>
2026-01-27 19:53:57 +0100

    ajouter les routes de recherche et affiche les utilisateurs profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserSearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/recherche', [UserSearchController::class, 'index'])
+        ->name('users.search');
+
+    Route::get('/profil/{user}', [ProfileController::class, 'show'])
+        ->name('users.show');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
